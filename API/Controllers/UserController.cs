c2fb2a6510b25672928818b72f8e4bee813efc86
AntakovAndrey <andrey.antakov05@gmail.com>
2024-09-19 19:02:10 +0300

    api initialized

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using API.Mocks;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return UserStorage.GetInstance().GetAllUsers();
+        }
+
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return UserStorage.GetInstance().GetUserById(id);
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ API/Mocks/UserStorage.cs
@@ -0,0 +1,44 @@
+﻿using API.Models;
+
+namespace API.Mocks
+{
+    public class UserStorage
+    {
+        private static UserStorage instance;
+        private List<User> users;
+
+        private UserStorage() 
+        {
+            users = new List<User> { new User { Id = 1,Name="Майкл Джексон",Email="moonwalk@gmail.com",Password="MJ_moonWalker007"},
+                                     new User { Id = 2,Name="",Email="",Password="" },
+                                     new User { Id = 3,Name="",Email="",Password="" },
+                                     new User { Id = 4,Name="",Email="",Password="" },
+                                     new User { Id = 5,Name="",Email="",Password="" }};
+        }
+
+        public static UserStorage GetInstance()
+        {
+            if (instance == null)
+                instance = new UserStorage();
+            return instance;
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return users;
+        }
+        public User GetUserById(int id)
+        {
+            return users.First(user => user.Id == id);
+        }
+        public void AddUser(User user)
+        {
+            users.Add(user);
+        }
+        public void DeleteUser(int id)
+        {
+            users.Remove(users.First(user=>user.Id == id));
+        }
+
+    }
+}
