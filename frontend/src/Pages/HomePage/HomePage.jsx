bd6cef181683315fc6b44bd02337bf076ad80666
AntakovAndrey <andrey.antakov05@gmail.com>
2024-10-10 13:06:24 +0300

    deleted api folder

--- frontend/src/Pages/HomePage/HomePage.jsx
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -7,9 +7,9 @@ function HomePage(){
         <>
             <Header/>
             <div className="body_container">
-                <div className={styles.start_page_slider} style={{backgroundColor:"#000",width:"100%",height:"580px",margin:"auto"}}>
+                <div className={styles.start_page_slider} style={{backgroundImage:'URL("Container.png")',backgroundRepeat: 'no-repeat',backgroundSize:'cover',background:'linear-gradient(rgba(255,0,0,0.1), #ebf8e1, #3f87a6)', backgroundColor:"#000",width:"100%",height:"580px",margin:"auto"}}>
                     <div className={styles.start_page_slider_item}>
-                        <img width="100%" src="Container.png" />
+                        
                     </div>
                 </div>
             </div>
