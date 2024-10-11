2321aee968cb75e6de2c5f37fe925a4bda56e939
AntakovAndrey <andrey.antakov05@gmail.com>
2024-10-11 14:21:46 +0300

    frontend clear react project

--- frontend/src/Components/Header/Search.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Search(props){
-    return(
-            <input style={{display:(props.isVisible?"inline":"none")}}type="text" />
-    )
-}
-
-export default Search;
