3d8fb2990f8a98a6403264ffb0d80c1c000dfadb
AntakovAndrey <andrey.antakov05@gmail.com>
2024-10-11 19:28:33 +0300

    Player logics added

--- frontend/src/Pages/Player.jsx
+++ frontend/src/Pages/Player.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-import {} from 'react-router-dom'
+import {useLocation} from 'react-router-dom'
 
 function Player(){
+    const location = useLocation();
+    const params = URLSearchParams(location.search)
+    const videoUrl = params.get("videoUrl")
+    
     return(
         <>
+            <h2>Воспроизведение видео</h2>
+            <video controls autoPlay width="100%">
+                <source src={videoUrl} type="video/mp4" />
+                Ваш браузер не поддерживает видео.
+            </video>
         </>
     )
 }
