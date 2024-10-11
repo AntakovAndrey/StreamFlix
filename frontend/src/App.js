61d722926df5bf6902533ce9a3156f996a4f5f06
AntakovAndrey <andrey.antakov05@gmail.com>
2024-10-11 19:33:14 +0300

    routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import React from "react";
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Upload from "./Pages/Upload"
+import Player from "./Pages/Player"
+
 function App() {  
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Upload/>}/>
+          <Route path="/player" element={<Player/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
