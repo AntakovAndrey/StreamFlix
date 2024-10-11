77f7c216d915362fadc4c46703833ad51d6fe3a2
AntakovAndrey <andrey.antakov05@gmail.com>
2024-10-11 23:40:33 +0300

    downloadAndStreamingAdded

--- backend/Api/Controllers/TorrentController.cs
+++ backend/Api/Controllers/TorrentController.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.FileProviders;
-using System.Net;
+﻿using Microsoft.AspNetCore.Mvc;
+using MonoTorrent.Client;
 
 namespace Api.Controllers
 {
@@ -13,13 +11,34 @@ namespace Api.Controllers
         public async Task<IActionResult> UploadTorrentFile(IFormFile file) 
         {
             string torrentFilePath = Path.Combine("uploads", file.FileName);
-
             using (var stream = new FileStream(torrentFilePath, FileMode.OpenOrCreate))
             {
                  file.CopyTo(stream);
             }
+            string resultFilePath = await StartTorrentDownload(torrentFilePath);
+            return Ok($"/stream?file={Uri.EscapeDataString(resultFilePath)}");
+        }
 
-            return Ok(file);
-        } 
+        [HttpGet("stream")]
+        public async Task<IActionResult> StreamVideo(string fileName)
+        {
+            string videoPath=Path.Combine("./downloads/", fileName);
+            var fileStream = new FileStream(videoPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            var response = new FileStreamResult(fileStream, "video/mp4");
+            response.EnableRangeProcessing = true;
+            return response;
+        }
+
+        private async Task<string> StartTorrentDownload(string torrentFilePath)
+        {
+            var engine = new ClientEngine();
+            var torrentManager = await engine.AddAsync(torrentFilePath,"./downloads/");
+            await torrentManager.StartAsync();
+            while(torrentManager.Progress<5)
+            {
+                await Task.Delay(100);
+            }
+            return torrentManager.Files.First(f=>f.Path.EndsWith(".mp4")).Path;
+        }
     }
 }
