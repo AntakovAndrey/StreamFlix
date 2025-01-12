d10e4cfde942d330caa2f281366a7e8f0fde7846
AntakovAndrey <andrey.antakov05@gmail.com>
2025-01-13 00:45:11 +0300

    IMovieTorrentLink created

---
+++ Backend/Services/ParsingService/Domain/ParsingService.Domain.Interfaces/IMovieTorrentLinksRepository.cs
@@ -0,0 +1,13 @@
+﻿using ParsingService.Domain.Core;
+
+namespace ParsingService.Domain.Interfaces;
+
+public interface IMovieTorrentLinksRepository
+{
+    public MovieTorrentLink GetMovieTorrentLinkById(int id);
+    public MovieTorrentLink GetMovieTorrentLinkByMovieId(int movieId);
+    public IEnumerable<MovieTorrentLink> GetMovieTorrentLinks();
+    public void AddMovieTorrentLink(MovieTorrentLink movie);
+    public void UpdateMovieTorrentLink(MovieTorrentLink movie);
+    public void DeleteMovieTorrentLink(int id);
+}
