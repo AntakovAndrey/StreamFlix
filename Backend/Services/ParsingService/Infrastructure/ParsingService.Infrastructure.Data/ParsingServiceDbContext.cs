586162bfe89f9346d7d3c01607f3981a3a93a02e
AntakovAndrey <andrey.antakov05@gmail.com>
2025-01-13 00:46:58 +0300

    ParsingServiceDBContext created

---
+++ Backend/Services/ParsingService/Infrastructure/ParsingService.Infrastructure.Data/ParsingServiceDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParsingService.Domain.Core;
+
+namespace ParsingService.Infrastructure.Data;
+
+public class ParsingServiceDbContext:DbContext
+{
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Series> Series { get; set; }
+    public DbSet<MovieTorrentLink> MovieTorrentLinks { get; set; }
+    public DbSet<SeriesTorrentLink> SeriesTorrentLinks { get; set; }
+
+    public ParsingServiceDbContext(DbContextOptions<ParsingServiceDbContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+}
