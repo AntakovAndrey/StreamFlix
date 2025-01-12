da8cce7cc91ff8373ce24e4b5a52388ec4b769b5
AntakovAndrey <andrey.antakov05@gmail.com>
2025-01-13 00:47:19 +0300

    MovieRepository created

---
+++ Backend/Services/ParsingService/Infrastructure/ParsingService.Infrastructure.Data/MovieRepository.cs
@@ -0,0 +1,42 @@
+﻿using ParsingService.Domain.Core;
+using ParsingService.Domain.Interfaces;
+
+namespace ParsingService.Infrastructure.Data;
+
+public class MovieRepository:IMovieRepository
+{
+    ParsingServiceDbContext _dbContext;
+    
+    public MovieRepository(ParsingServiceDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public Movie GetMovieById(int id)
+    {
+        return _dbContext.Movies.Find(id);
+    }
+
+    public IEnumerable<Movie> GetMovies()
+    {
+        return _dbContext.Movies;
+    }
+
+    public void AddMovie(Movie movie)
+    {
+        _dbContext.Movies.Add(movie);
+        _dbContext.SaveChanges();
+    }
+
+    public void UpdateMovie(Movie movie)
+    {
+        _dbContext.Movies.Update(movie);
+        _dbContext.SaveChanges();
+    }
+
+    public void DeleteMovie(int id)
+    {
+        _dbContext.Movies.Remove(GetMovieById(id));
+        _dbContext.SaveChanges();
+    }
+}
