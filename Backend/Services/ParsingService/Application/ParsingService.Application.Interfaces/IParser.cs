ab15cbffcaef6b9eabbb45740ef64c03761f67b1
AntakovAndrey <andrey.antakov05@gmail.com>
2025-01-13 01:02:10 +0300

    IParses and IParserContainer added

---
+++ Backend/Services/ParsingService/Application/ParsingService.Application.Interfaces/IParser.cs
@@ -0,0 +1,9 @@
+﻿using ParsingService.Domain.Core;
+
+namespace ParsingService.Application.Interfaces;
+
+public interface IParser
+{
+    public string BaseUrl { get; }
+    public IEnumerable<ParseResult> Parse();
+}
---
+++ Backend/Services/ParsingService/Application/ParsingService.Application.Interfaces/IParsersContainer.cs
@@ -0,0 +1,8 @@
+﻿namespace ParsingService.Application.Interfaces;
+
+public interface IParsersContainer
+{
+    public void RegisterParser(IParser parser);
+    public IEnumerable<IParser> GetParsers();
+    public void RunParsers();
+}
