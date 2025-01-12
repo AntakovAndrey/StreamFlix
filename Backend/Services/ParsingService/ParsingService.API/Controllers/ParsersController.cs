bc929ae651b47af92a50a7d19d20bd5ad0daf19c
AntakovAndrey <andrey.antakov05@gmail.com>
2025-01-13 00:52:08 +0300

    Parsers controller added

---
+++ Backend/Services/ParsingService/ParsingService.API/Controllers/ParsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ParsingService.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ParsersController:Controller
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hello World!");
+    }
+}
