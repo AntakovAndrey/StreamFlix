66c61d14aa1f763d74d8ea035f41bc38ee88fd64
AntakovAndrey <andrey.antakov05@gmail.com>
2025-01-13 00:44:46 +0300

    IMovieRepository created

---
+++ Backend/Services/ParsingService/Domain/ParsingService.Domain.Interfaces/IMovieRepository.cs
@@ -0,0 +1,12 @@
+﻿using ParsingService.Domain.Core;
+
+namespace ParsingService.Domain.Interfaces;
+
+public interface IMovieRepository
+{
+    public Movie GetMovieById(int id);
+    public IEnumerable<Movie> GetMovies();
+    public void AddMovie(Movie movie);
+    public void UpdateMovie(Movie movie);
+    public void DeleteMovie(int id);
+}
