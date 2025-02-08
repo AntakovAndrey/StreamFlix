16decc60b16fca443d55454b1326adee3f9a5af3
AntakovAndrey <andrey.antakov05@gmail.com>
2025-02-09 00:46:55 +0300

    Movie class added

--- Backend/Services/ParsingService/Domain/ParsingService.Domain.Core/Movie.cs
+++ Backend/Services/ParsingService/Domain/ParsingService.Domain.Core/Movie.cs
@@ -5,4 +5,8 @@ public class Movie:ParseResult
     public int Id { get; set; }
     public string Title { get; set; }
     public IEnumerable<MovieTorrentLink> MovieTorrentLinks { get; set; }
+    public override string ToString()
+    {
+        return $"{Title} {MovieTorrentLinks.First().Link}";
+    }
 }
