9475b500a4fa17013370f39b57d92630e2e8bb96
AntakovAndrey <andrey.antakov05@gmail.com>
2025-02-11 19:37:34 +0300

    IParser interface added

--- Backend/Services/ParsingService/Application/ParsingService.Application.Interfaces/IParser.cs
+++ Backend/Services/ParsingService/Application/ParsingService.Application.Interfaces/IParser.cs
@@ -1,9 +1,19 @@
 ﻿using ParsingService.Domain.Core;
+using ParsingService.Domain.Core.Enums;
 
 namespace ParsingService.Application.Interfaces;
 
 public interface IParser
 {
+    public Guid Guid { get; set; }
+    public string Name { get;}
     public string BaseUrl { get; }
-    public Task<IEnumerable<ParseResult>> ParseAsync();
+    public ParserStatus Status { get; set; }
+    public DateTime LastStarted { get; set; }
+    public void ParseAsync();
+    public event ParseHandler OnParse;
+    public delegate void ParseHandler(IEnumerable<ParseResult> results);
+    
+    public void Start();
+    protected Thread CurrentThread { get; set; }
 }
