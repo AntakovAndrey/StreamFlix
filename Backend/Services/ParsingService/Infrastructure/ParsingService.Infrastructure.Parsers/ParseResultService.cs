0404f326e68b75bc162bab38bd2de1c4188236ad
AntakovAndrey <andrey.antakov05@gmail.com>
2025-02-16 20:01:41 +0300

    ParserService

---
+++ Backend/Services/ParsingService/Infrastructure/ParsingService.Infrastructure.Parsers/ParseResultService.cs
@@ -0,0 +1,19 @@
+﻿using ParsingService.Domain.Core;
+using ParsingService.Domain.Interfaces;
+using ParsingService.Infrastructure.Data;
+
+namespace ParsingService.Infrastructure.Parsers;
+
+public class ParseResultService
+{
+    private readonly IMovieRepository _movieRepository;
+    public ParseResultService(MovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+
+    public async Task VerifyParseResult(IEnumerable<ParseResult> results)
+    {
+        return;
+    }
+}
