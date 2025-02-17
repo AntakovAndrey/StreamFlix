00349126d910d83e59a615601223ea523103b51f
AntakovAndrey <andrey.antakov05@gmail.com>
2025-02-17 22:21:53 +0300

    IGenreRepositry added

---
+++ Backend/Services/MovieService/Domain/MovieService.Domain.Interfaces/IGenreRepository.cs
@@ -0,0 +1,12 @@
+﻿using MovieService.Domain.Core;
+
+namespace MovieService.Domain.Interfaces;
+
+public interface IGenreRepository
+{
+    public IEnumerable <Genre> GetGenres();
+    public Genre GetGenreById(int id);
+    public Genre AddProduct(Genre product);
+    public Genre UpdateProduct(Genre product);
+    public bool DeleteProduct(int id);
+}
