7c3b057af60abd0c4630414d1a2e7a415d0b9cc9
AntakovAndrey <andrey.antakov05@gmail.com>
2025-02-17 22:25:41 +0300

    MovieServiceDbContext added

---
+++ Backend/Services/MovieService/Infrastructure/MovieService.Infrastructure.Data/MovieServiceDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieService.Domain.Core;
+
+namespace MovieService.Infrastructure.Data;
+
+public class MovieServiceDbContext:DbContext
+{
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+    
+    public MovieServiceDbContext(DbContextOptions<MovieServiceDbContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+}
