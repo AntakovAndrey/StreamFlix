98a7d4424467e454f3e8ab1a7a85be6ed37977cd
AntakovAndrey <andrey.antakov05@gmail.com>
2025-02-17 11:31:16 +0300

    Movie controller added

---
+++ Backend/Services/MovieService/MovieService.API/Controllers/MovieController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieService.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MovieController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok();
+    }
+}
