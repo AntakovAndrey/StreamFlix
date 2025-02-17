c7a9ef5811a974f56695d5e7287604d39c4ede51
AntakovAndrey <andrey.antakov05@gmail.com>
2025-02-17 13:07:31 +0300

    Movie service domain core

---
+++ Backend/Services/MovieService/Domain/MovieService.Domain.Core/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieService.Domain.Core;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? OriginalTitle { get; set; }
+    public int ReleaseYear { get; set; }
+}
