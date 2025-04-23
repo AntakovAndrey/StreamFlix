0bd8f1db9f0f6de4f5215fc17f77da9e10d7f0ec
AntakovAndrey <andrey.antakov05@gmail.com>
2025-04-23 19:47:37 +0300

    IUserRepository added

--- Backend/Services/UserService/Domain/UserService.Domain.Interfaces/IUserRepository.cs
+++ Backend/Services/UserService/Domain/UserService.Domain.Interfaces/IUserRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace UserService.Domain.Interfaces;
+﻿using UserService.Domain.Core;
+
+namespace UserService.Domain.Interfaces;
 
 public interface IUserRepository
 {
-    
+    User? GetUser(int id);
+    IEnumerable<User> GetUsers();
+    void Create(User user);
+    void Update(User user);
+    void Delete(int id);
+    void Save();
 }
